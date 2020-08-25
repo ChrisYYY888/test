6efbee00b0430afdc76dbf667983265eced984ca
yinqiang <yinqiang@foryou56.com>
2020-08-25 17:56:11 +0800

    beautifulReport

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
-
 # 基础路径
 import os
+import time
 import unittest
-
+from BeautifulReport import BeautifulReport
+from HTMLTestRunner import HTMLTestRunner
 from HTMLTestRunner_Echarts import HTMLTestRunner_Echarts
 
 base_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '')
@@ -15,11 +16,15 @@ def run_test():
     filename = base_path + 'report/report_test_gd.html'
     fp = open(filename, 'wb')
     title = '接口自动化测试报告'
-    # runner = HTMLTestRunner(stream=fp, title=title, description='Foryou Testing', verbosity=2, tester='yinqiang')
-    # runner.run(discover)
-    # 饼状图报告
-    runner_echarts = HTMLTestRunner_Echarts(stream=fp, title=title, description='Interface AutoTest', verbosity=2)
-    runner_echarts.run(discover)
+    #
+    runner = HTMLTestRunner(stream=fp, title=title, description='Foryou Testing', verbosity=2, tester='yinqiang')
+    runner.run(discover)
+    # # 饼状图报告
+    # runner_echarts = HTMLTestRunner_Echarts(stream=fp, title=title, description='Interface AutoTest', verbosity=2)
+    # runner_echarts.run(discover)
+    # # BeautifulReport
+    # result = BeautifulReport(discover)
+    # result.report(filename=time.strftime("%Y-%m-%d %H:%M:%S") + '测试报告', description='*****报告', report_dir=base_path + '/report')
     fp.close()
 
 
