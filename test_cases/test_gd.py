7024cffbd32fa10ef2c6e534b7bd79ab6471a6b2
yinqiang <yinqiang@foryou56.com>
2020-08-31 17:31:16 +0800

    fix

--- test_cases/test_gd.py
+++ test_cases/test_gd.py
@@ -12,6 +12,20 @@ class TestGD(unittest.TestCase):
         self.assertEqual(self.r['status'], "1")
         self.assertEqual(self.r['info'], "OK")
 
+    def test_2(self):
+        """接口测试脚本2"""
+        print("test2")
+        self.a = 1
+        self.assertEqual(self.a, 1)
+
+    def test_3(self):
+        """接口测试脚本3"""
+        print("test3")
+        self.a = 1
+        self.b = "2"
+        self.c = self.a + self.b
+        self.assertEqual(self.c, 3)
+
 
 if __name__ == '__main__':
     unittest.main()
