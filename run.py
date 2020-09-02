250b2fdc9767392d614b0712f0ffbb9ac2e39762
yinqiang <yinqiang@foryou56.com>
2020-09-02 16:44:47 +0800

    beautifulreport

--- run.py
+++ run.py
@@ -19,19 +19,19 @@ def run_test():
     fp = open(filename, 'wb')
     title = '接口自动化测试报告'
     #
-    # runner = HTMLTestRunner(stream=fp,
-    #                         title=title,
-    #                         description='执行环境：系统：' + platform.platform() + '，Python版本：' + sys.version,
-    #                         verbosity=2,
-    #                         tester='yinqiang')
-    # runner.run(discover)
+    runner = HTMLTestRunner(stream=fp,
+                            title=title,
+                            description='执行环境：系统：' + platform.platform() + '，Python版本：' + sys.version,
+                            verbosity=2,
+                            tester='yinqiang')
+    runner.run(discover)
     # # 饼状图报告
-    runner_echarts = HTMLTestRunner_Echarts(stream=fp,
-                                            title=title,
-                                            description='Interface AutoTest,执行环境：系统：' + platform.platform() +
-                                                        '，Python版本：' + sys.version,
-                                            verbosity=2)
-    runner_echarts.run(discover)
+    # runner_echarts = HTMLTestRunner_Echarts(stream=fp,
+    #                                         title=title,
+    #                                         description='Interface AutoTest,执行环境：系统：' + platform.platform() +
+    #                                                     '，Python版本：' + sys.version,
+    #                                         verbosity=2)
+    # runner_echarts.run(discover)
     # # BeautifulReport
     # result = BeautifulReport(discover)
     # result.report(filename=time.strftime("%Y-%m-%d %H:%M:%S") + '测试报告',
