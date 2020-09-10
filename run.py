3a25eb9e520241b43fe9dd55232e908e27b5ae65
yinqiang <yinqiang@foryou56.com>
2020-09-10 10:19:56 +0800

    run sys.argv

--- run.py
+++ run.py
@@ -12,14 +12,15 @@ base_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '')
 print(base_path)
 
 
-def run_test(report_type=1):
+def run_test(argv):
+    print(argv)
     case_dir = base_path + 'test_cases/'
     discover = unittest.defaultTestLoader.discover(case_dir, pattern='test_*.py')
     filename = base_path + 'report/report_test_gd.html'
     fp = open(filename, 'wb')
     title = '接口自动化测试报告'
     # HTMLTestRunner
-    if report_type == 1:
+    if argv[1] == '1':
         runner = HTMLTestRunner(stream=fp,
                                 title=title,
                                 description='执行环境：系统：' + platform.platform() + '，Python版本：' + sys.version,
@@ -27,7 +28,7 @@ def run_test(report_type=1):
                                 tester='yinqiang')
         result = runner.run(discover)
     # 饼状图报告
-    elif report_type == 2:
+    elif argv[1] == '2':
         runner_echarts = HTMLTestRunner_Echarts(stream=fp,
                                                 title=title,
                                                 description='Interface AutoTest,执行环境：系统：' + platform.platform() +
@@ -35,7 +36,7 @@ def run_test(report_type=1):
                                                 verbosity=2)
         result = runner_echarts.run(discover)
     # BeautifulReport
-    elif report_type == 3:
+    elif argv[1] == '3':
         result = BeautifulReport(discover)
         result.report(filename=time.strftime("%Y-%m-%d %H:%M:%S") + '测试报告',
                       description='*****报告' + ' 执行环境：系统：' + platform.platform() + '，Python版本：' + sys.version,
@@ -46,14 +47,14 @@ def run_test(report_type=1):
         f = open(file, 'w')
         runner = unittest.TextTestRunner(stream=f)
         result = runner.run(discover)
-    print("all cases number: {}".format(result.testsRun))
-    print("failed cases number: {}".format(result.failure_count))
-    print("failed cases reason: {}".format(result.failures))
-    print("error cases number: {}".format(result.error_count))
-    print("error cases reason: {}".format(result.errors))
-    print("success cases number: {}".format(result.success_count))
+    # print("all cases number: {}".format(result.testsRun))
+    # print("failed cases number: {}".format(result.failure_count))
+    # print("failed cases reason: {}".format(result.failures))
+    # print("error cases number: {}".format(result.error_count))
+    # print("error cases reason: {}".format(result.errors))
+    # print("success cases number: {}".format(result.success_count))
     fp.close()
 
 
 if __name__ == '__main__':
-    run_test()
+    run_test(sys.argv)
